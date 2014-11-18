c35fc2ba733ab244ec6e3080239ad3ecf1c82ba6
Kyle Bernstein <kyle@sitescout.com>
2014-11-18 14:58:24 -0500

    Initial Commit

---
+++ Gameboy/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Gameboy
+//
+//  Created by Kyle Bernstein on 11/18/14.
+//  Copyright (c) 2014 Kyle Bernstein. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
